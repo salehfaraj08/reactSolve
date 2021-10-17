24e7a3d20f286d1f25223b0dc6a82cfbe0770e8b
salehfaraj08 <saleh.faraj08@gmail.com>
2021-10-17 22:57:26 +0300

    ex3-1boxes

---
+++ 3-1boxes/src/App.js
@@ -0,0 +1,12 @@
+import Box1 from "./components/3-1boxes/box1";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Box1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 3-1boxes/src/components/3-1boxes/box3.js
@@ -0,0 +1,11 @@
+import Box4 from './box4';
+import './boxes.css'
+
+const Box3 = ()=>{
+    return <div className="box3">
+        <Box4/>
+        <Box4/>
+    </div>;
+}
+
+export default Box3;
